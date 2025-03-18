77b812896becd5d73cfddb14de962b841abed8bd
spenc3004 <knei294391@alvernia.edu>
2025-03-18 11:00:52 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3001
+
+//let data = require('./docs/response.json')
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send('Hello world!')
+})
+
+
+app.post("/ss", (req, res) => {
+    const data = req.body
+    console.log(data)
+    res.status(200).send("OK")
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`This is running on port http://localhost:${PORT}`)
+})
