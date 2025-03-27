2a1540b95d00d9591d90018b9b5179cef9ca202d
spenc3004 <knei294391@alvernia.edu>
2025-03-27 11:18:17 -0400

    Formatted acording to new template from Sarah. Pulled company name into email subject line.

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ app.post("/ss", async (req, res) => {
     // #region Receive JSON from Survey Sparrow
     try {
         const data = req.body
+        const company = data.companyName
         const htmlContent = generateHTML(data)
-        const htmlFilePath = path.join(OUTPUT_DIR, 'output.html');
-        const docxFilePath = path.join(OUTPUT_DIR, 'output.docx');
+        const htmlFilePath = path.join(OUTPUT_DIR, 'HVAC_Brief.html');
+        const docxFilePath = path.join(OUTPUT_DIR, 'HVAC_Brief.docx');
 
         fs.writeFileSync(htmlFilePath, htmlContent);
         await convertToDocx(htmlFilePath, docxFilePath);
-        await sendEmail(docxFilePath);
+        await sendEmail(docxFilePath, company);
 
         fs.unlinkSync(htmlFilePath);
         fs.unlinkSync(docxFilePath);
@@ -46,134 +47,143 @@ app.post("/ss", async (req, res) => {
 
 function generateHTML(data) {
     // #region insert data dynamically to html
+    const photoLinks = data.photos
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View Photo</i></u></a>`)
+        .join("<br>");
+    // dynamically create a tags for logo photos
+    const logoLinks = data.logo
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View Logo</i></u></a>`)
+        .join("<br>");
+    // dynamically create a tags for awar or logo photos
+    const awardOrLogoLinks = data.awards
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View Logo</i></u></a>`)
+        .join("<br>");
+    // dynamically create a tags for bbb logo photos
+    const bbbLinks = data.bbb
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View BBB Logo</i></u></a>`)
+        .join("<br>");
+
+    const phone = `(${data.companyPhone.slice(3, 6)})-${data.companyPhone.slice(7, 10)}-${data.companyPhone.slice(11)}`
+
+    function numCoupons() {
+        counter = 0
+        for (const key in data.radiusOffers) {
+            if (key.startsWith("coupon")) {
+                if (data.radiusOffers[key] !== "null") {
+                    counter += 1
+                }
+            }
+        }
+        return counter;
+    }
+
+
     return `<!DOCTYPE html>
     <html lang="en">
     
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>HVAC Brief</title>
     </head>
     
     <body>
-            <p>We have a new HVAC client doing a "Insert Product and Size"</p>
-            <p><strong><u>DESIGN:</u></strong> Specific design? ${data.design} Design based on look of website? ${data.designBasedOnWeb}</p>
-            <p><strong><u>LOCATION INFORMATION:</u></strong><br>
-            Address: <br>
+            <h4>We have a new HVAC client doing a "Insert Product and Size"</h4>
+            <h5><u>DESIGN:</u></h5>
+            ${data.design === "true" ? `<p>There is a specific design.</p> ` : ``}
+            ${data.designBasedOnWeb === "true" ? `<p>Base the design on their website.</p> ` : ``}
+
+            <h5><u>LOCATION INFORMATION:</u></h5>
             ${data.companyName}<br>
             ${data.companyAddress}<br>
             ${data.companyAddress2}<br>
-            ${data.companyCity}, ${data.companyState} ${data.companyZip}<br>
-            “Insert Call to Action” Based on Q4<br>
-            Call Today to Schedule Your Appointment! 
-            OR Call Today or Conveniently Schedule Online! 
-            OR Call Today or Conveniently Schedule Online!  (Insert QR Code) Scan Here to Easily Schedule Your Appointment! (Q4)
-            Phone: ${data.companyPhone}<br>
-            Website: ${data.website}<br>
-            Hours: <br>
-            License #: ${data.license}
-            </p>
-            <p><strong><u>PHOTOS TO USE:</u></strong> ${data.photos}</p>
-            <p><strong><u>SERVICES:</u></strong> ${data.services}</p>
-            <p><strong><u>You Can Trust Us to Do the Job for You:</u></strong> ${data.pricing}, ${data.warranties}, ${data.technicians}, ${data.financing}</p>
-            <p><strong><u>TAGLINES:</u></strong> ${data.hasTaglines}<br>
-            (Side #1)- ${data.taglines.tagline1}<br>
-            (Side #2)- ${data.taglines.tagline2}
-            </p>
-            <p><strong><u>RATINGS:</u></strong><br>
+            ${data.companyCity}, ${data.companyState} ${data.companyZip}<br><br>
+            ${phone}<br><br>
+            ${data.website}<br><br>
+            ${data.license}
+            ${data.onlineService === "true" ? `<p>Call Today to Schedule Your Appointment!</p>` : `
+                <p><strong>“Insert Call to Action” Based on Q4</strong></p>
+                OR Call Today or Conveniently Schedule Online! <br>
+                OR Call Today or Conveniently Schedule Online!  (Insert QR Code) Scan Here to Easily Schedule Your Appointment!`}
+            <h5><u>PHOTOS TO USE:</u></h5>
+            ${photoLinks}
+            <h5><u>SERVICES:</u></h5>
+            ${data.services.split(",").join("<br>")}
+            <h5><u>You Can Trust Us to Do the Job for You:</u></h5>
+            ${data.pricing}<br>
+            ${data.warranties}<br>
+            ${data.technicians}<br>
+            ${data.financing}
+            <h5><u>TAGLINES:</u></h5>
+            ${data.taglines.tagline1}<br>
+            ${data.taglines.tagline2}<br>
+
+            <h5><u>RATINGS:</u></h5>
             Google: ${data.stars.google}<br>
             ${data.stars.other1}<br>
             ${data.stars.other2}<br>
             ${data.stars.other3}<br>
-            ${data.stars.other4}<br>
-            <p><strong><u>LOGOs to Use:</u></strong> Logo: ${data.logo}<br>
-            BBB: ${data.bbb}<br>
-            Logo or Award: ${data.awards}<br>
-            Other Awards, Affiliations, or Organizations: ${data.otherAwards}
-            </p>
-            <p><strong><u>OTHER NOTES:</u></strong> <br>
-            Applicable: ${data.applicables}<br>
-            Radius Offers: <br>
-            <table>
-                <tr>
-                    <th>Coupon</th>
-                    <th>Disclaimer</th>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon1}</td>
-                    <td>${data.radiusOffers.disclaimer1}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon2}</td>
-                    <td>${data.radiusOffers.disclaimer2}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon3}</td>
-                    <td>${data.radiusOffers.disclaimer3}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon4}</td>
-                    <td>${data.radiusOffers.disclaimer4}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon5}</td>
-                    <td>${data.radiusOffers.disclaimer5}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon6}</td>
-                    <td>${data.radiusOffers.disclaimer6}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon7}</td>
-                    <td>${data.radiusOffers.disclaimer7}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon8}</td>
-                    <td>${data.radiusOffers.disclaimer8}</td>
-                </tr>
-            </table>
+            ${data.stars.other4}
+            <h5><u>LOGOs to Use:</u></h5> 
+            ${!data.logo ? `` : `${logoLinks}<br>`}
+            ${!data.bbb ? `` : `${bbbLinks}<br>`}
+            ${!data.awards ? `` : `${awardOrLogoLinks}<br>`}
+            ${data.otherAwards !== "null" ? `<h6>Other Awards, Affiliations, or Organizations:</h6>  
+            ${data.otherAwards.split(",").join("<br>")}` : ``}
+            <h5><u>OTHER NOTES:</u></h5> 
+            ${data.applicables}
+           
             ${JSON.parse(data.homeOwner) ? `
-               New Homeowner Mailings:</p>
-            <p><strong><u>COUPONS(There are "4" coupons):</u></strong><br>           <table>
-                <tr>
-                    <th>Coupon</th>
-                    <th>Disclaimer</th>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon1}</td>
-                    <td>${data.radiusOffers.disclaimer1}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon2}</td>
-                    <td>${data.radiusOffers.disclaimer2}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon3}</td>
-                    <td>${data.radiusOffers.disclaimer3}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon4}</td>
-                    <td>${data.radiusOffers.disclaimer4}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon5}</td>
-                    <td>${data.radiusOffers.disclaimer5}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon6}</td>
-                    <td>${data.radiusOffers.disclaimer6}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon7}</td>
-                    <td>${data.radiusOffers.disclaimer7}</td>
-                </tr>
-                <tr>
-                    <td>${data.radiusOffers.coupon8}</td>
-                    <td>${data.radiusOffers.disclaimer8}</td>
-                </tr>
-            </table></p> 
-                
+                <h6>New Home Owner mailings?</h6> Yes
+                <h6>Radius Offers: </h6>
+                <table>
+                    <tr>
+                        <th>Coupon</th>
+                        <th>Disclaimer</th>
+                    </tr>
+                    ${data.radiusOffers.coupon1 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon1}</td>
+                        <td>${data.radiusOffers.disclaimer1}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon2 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon2}</td>
+                        <td>${data.radiusOffers.disclaimer2}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon3 !== "null" ? `
+                        <tr>
+                            <td>${data.radiusOffers.coupon3}</td>
+                            <td>${data.radiusOffers.disclaimer3}</td>
+                        </tr>`: ``}
+                </table></p> 
+              
             ` : ``}
+            <h5><u>COUPONS(There are ${numCoupons()} coupons):</u></h5>        
+             <table>
+                    <tr>
+                        <th>Coupon</th>
+                        <th>Disclaimer</th>
+                    </tr>
+                    ${data.radiusOffers.coupon1 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon1}</td>
+                        <td>${data.radiusOffers.disclaimer1}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon2 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon2}</td>
+                        <td>${data.radiusOffers.disclaimer2}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon3 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon3}</td>
+                        <td>${data.radiusOffers.disclaimer3}</td>
+                    </tr>`: ``}
+                </table></p> 
         </body>
     </body>
     
@@ -197,7 +207,7 @@ function convertToDocx(htmlFilePath, docxFilePath) {
     // #endregion
 }
 
-async function sendEmail(attachmentPath) {
+async function sendEmail(attachmentPath, company) {
     // #region Send output.docx in email
 
     let transporter = nodemailer.createTransport({
@@ -214,7 +224,7 @@ async function sendEmail(attachmentPath) {
     let mailOptions = {
         from: process.env.user,
         to: process.env.recipient,
-        subject: 'New HVAC Survey Submitted',
+        subject: `New HVAC Survey Submitted for ${company}`,
         text: 'Please see the attached document.',
         attachments: [{ path: attachmentPath }]
     };
