9e7d7d5487baf49776550ca6bfa330da9716f1b2
spenc3004 <knei294391@alvernia.edu>
2025-04-15 11:59:10 -0400

    Change endpoint from /ss to /havc. Add auto end point. HVAC handle BBB logo based on labels.

--- server.js
+++ server.js
@@ -19,18 +19,19 @@ app.get("/", (req, res) => {
 })
 
 
-app.post("/ss", async (req, res) => {
+app.post("/hvac", async (req, res) => {
     // #region Receive JSON from Survey Sparrow
     try {
         const data = req.body
         const company = data.companyName
-        const htmlContent = generateHTML(data)
+        const type = "HVAC"
+        const htmlContent = generateHvacHTML(data)
         const htmlFilePath = path.join(OUTPUT_DIR, 'HVAC_Brief.html');
         const docxFilePath = path.join(OUTPUT_DIR, 'HVAC_Brief.docx');
 
         fs.writeFileSync(htmlFilePath, htmlContent);
         await convertToDocx(htmlFilePath, docxFilePath);
-        await sendEmail(docxFilePath, company);
+        await sendEmail(docxFilePath, company, type);
 
         fs.unlinkSync(htmlFilePath);
         fs.unlinkSync(docxFilePath);
@@ -45,30 +46,54 @@ app.post("/ss", async (req, res) => {
     // #endregion
 });
 
-function generateHTML(data) {
-    // #region insert data dynamically to html
+app.post("/auto", async (req, res) => {
+    // #region Receive JSON from Survey Sparrow
+    try {
+        const data = req.body
+        const company = data.companyName
+        const type = "Auto"
+        const htmlContent = generateAutoHTML(data)
+        const htmlFilePath = path.join(OUTPUT_DIR, 'Auto_Brief.html');
+        const docxFilePath = path.join(OUTPUT_DIR, 'Auto_Brief.docx');
+
+        fs.writeFileSync(htmlFilePath, htmlContent);
+        await convertToDocx(htmlFilePath, docxFilePath);
+        await sendEmail(docxFilePath, company, type);
+
+        // fs.unlinkSync(htmlFilePath);
+        //fs.unlinkSync(docxFilePath);
+
+        res.status(200).send('File processed and email sent.');
+    }
+
+    catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+    // #endregion
+});
+
+function generateHvacHTML(data) {
+    // #region insert data dynamically into html
     const photoLinks = data.photos
         .split(",")
         .map(photo => `<a href="${photo}" target="_blank"><u><i>View Photo</i></u></a>`)
         .join("<br>");
-    // dynamically create a tags for logo photos
+    // dynamically create "a" tags for logo photos
     const logoLinks = data.logo
         .split(",")
         .map(photo => `<a href="${photo}" target="_blank"><u><i>View Logo</i></u></a>`)
         .join("<br>");
-    // dynamically create a tags for awar or logo photos
+    // dynamically create "a" tags for award or logo photos
     const awardOrLogoLinks = data.awards
         .split(",")
         .map(photo => `<a href="${photo}" target="_blank"><u><i>View Logo</i></u></a>`)
         .join("<br>");
-    // dynamically create a tags for bbb logo photos
-    const bbbLinks = data.bbb
-        .split(",")
-        .map(photo => `<a href="${photo}" target="_blank"><u><i>View BBB Logo</i></u></a>`)
-        .join("<br>");
 
-    const phone = `(${data.companyPhone.slice(3, 6)}) ${data.companyPhone.slice(7, 10)}-${data.companyPhone.slice(11)}`
 
+    const phone = `${data.companyPhone.slice(3, 6)}-${data.companyPhone.slice(7, 10)}-${data.companyPhone.slice(11)}`
+
+    // counts number of coupons in radiusOffers to then display in the HTML
     function numCoupons() {
         counter = 0
         for (const key in data.radiusOffers) {
@@ -106,7 +131,165 @@ function generateHTML(data) {
                 OR Call Today or Conveniently Schedule Online! <br>
                 OR Call Today or Conveniently Schedule Online!  (Insert QR Code) Scan Here to Easily Schedule Your Appointment!`}
             <h5><u>PHOTOS TO USE:</u></h5>
+            ${data.vehicles}
             ${photoLinks}
+            <h5><u>You Can Trust Us to Do the Job for You:</u></h5>
+            ${data.aseTechnicians}<br>
+            ${data.warranties}<br>
+            ${data.shuttleLoanerService}<br>
+            ${data.financing}<br>
+            ${data.amenities}<br>
+            ${data.sameDayService}<br>
+            ${data.approveFirst}
+            <h5><u>TAGLINES:</u></h5>
+            ${data.tagline1 !== "null" ? `${data.tagline1}<br>` : ``}
+            ${data.taglines !== "null" ? `${data.taglines.split(",").join("<br>")}` : ``}
+            <h5><u>RATINGS:</u></h5>
+            ${data.stars.google !== "null" ? `Google: ${data.stars.google}<br>` : ``}
+            ${data.stars.other1 !== "null" ? `${data.stars.other1}<br>` : ``}
+            ${data.stars.other2 !== "null" ? `${data.stars.other2}<br>` : ``}
+            ${data.stars.other3 !== "null" ? `${data.stars.other3}<br>` : ``}
+            <h5><u>LOGOs to Use:</u></h5> 
+            ${!data.logo ? `` : `${logoLinks}<br>`}
+            ${data.bbb !== "" ? `<p>BBB Logo: ${data.bbb}</p><br>` : ``}
+            ${!data.awards ? `` : `${awardOrLogoLinks}<br>`}
+            ${data.otherAwards !== "null" ? `<h6>Other Awards, Affiliations, or Organizations:</h6>  
+            ${data.otherAwards.split(",").join("<br>")}` : ``}
+            <h5><u>OTHER NOTES:</u></h5> 
+            ${data.applicables}
+           
+            ${JSON.parse(data.homeOwner) ? `
+                <h6>New Home Owner mailings?</h6> Yes
+                <h6>Radius Offers: </h6>
+                <table>
+                    <tr>
+                        <th>Coupon</th>
+                        <th>Disclaimer</th>
+                    </tr>
+                    ${data.radiusOffers.coupon1 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon1}</td>
+                        <td>${data.radiusOffers.disclaimer1}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon2 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon2}</td>
+                        <td>${data.radiusOffers.disclaimer2}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon3 !== "null" ? `
+                        <tr>
+                            <td>${data.radiusOffers.coupon3}</td>
+                            <td>${data.radiusOffers.disclaimer3}</td>
+                        </tr>`: ``}
+                </table></p> 
+              
+            ` : ``}
+            <h5><u>COUPONS(There are ${numCoupons()} coupons):</u></h5>        
+             <table>
+                    <tr>
+                        <th>Coupon</th>
+                        <th>Disclaimer</th>
+                    </tr>
+                    ${data.radiusOffers.coupon1 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon1}</td>
+                        <td>${data.radiusOffers.disclaimer1}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon2 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon2}</td>
+                        <td>${data.radiusOffers.disclaimer2}</td>
+                    </tr>`: ``}
+                    ${data.radiusOffers.coupon3 !== "null" ? `
+                    <tr>
+                        <td>${data.radiusOffers.coupon3}</td>
+                        <td>${data.radiusOffers.disclaimer3}</td>
+                    </tr>`: ``}
+                </table></p> 
+        </body>
+    </body>
+    
+    </html>`
+    // #endregion 
+}
+
+function generateAutoHTML(data) {
+    // #region insert data dynamically into html
+    const photoLinks = data.photos
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View Photo</i></u></a>`)
+        .join("<br>");
+    // dynamically create "a" tags for logo photos
+    const logoLinks = data.logo
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View Logo</i></u></a>`)
+        .join("<br>");
+    // dynamically create "a" tags for award or logo photos
+    const awardOrLogoLinks = data.awards
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View Logo</i></u></a>`)
+        .join("<br>");
+    // dynamically create "a" tags for bbb logo photos
+    const bbbLinks = data.bbb
+        .split(",")
+        .map(photo => `<a href="${photo}" target="_blank"><u><i>View BBB Logo</i></u></a>`)
+        .join("<br>");
+
+    const phone = `${data.companyPhone.slice(3, 6)}-${data.companyPhone.slice(7, 10)}-${data.companyPhone.slice(11)}`
+
+    // counts number of coupons in radiusOffers to then display in the HTML
+    function numCoupons() {
+        counter = 0
+        for (const key in data.coupons) {
+            if (key.startsWith("coupon")) {
+                if (data.coupons[key] !== "null") {
+                    counter += 1
+                }
+            }
+        }
+        return counter;
+    }
+
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    
+    <body>
+            <h4>We have a new HVAC client doing a "Insert Product and Size"</h4>
+            <h5><u>DESIGN:</u></h5>
+            ${data.design === "true" ? `<p>There is a specific design.</p> ` : ``}
+            ${data.designBasedOnWeb === "true" ? `<p>Base the design on their website.</p> ` : ``}
+
+            <h5><u>LOCATION INFORMATION:</u></h5>
+            ${data.companyName}<br><br>
+            ${data.companyAddress}<br><br>
+            ${data.companyAddress2 !== "null" ? `${data.companyAddress2}<br><br>` : ``}
+            ${data.companyCity}, ${data.companyState} ${data.companyZip}<br><br>
+            ${data.onlineService === "true" ? `<p>Call Today to Schedule Your Appointment!</p>` : `
+                <p><strong>“Insert Call to Action” Based on Q4</strong></p>
+                OR Call Today or Conveniently Schedule Online! <br>
+                OR Call Today or Conveniently Schedule Online!  (Insert QR Code) Scan Here to Easily Schedule Your Appointment!`}
+            ${phone}<br><br>
+            ${data.website}<br><br>
+            <p>Hours:</p> <br />
+            ${data.hours.Monday !== "null" ? `${data.hours.Monday}<br>` : ``}
+            ${data.hours.Tuesday !== "null" ? `${data.hours.Tuesday}<br>` : ``}
+            ${data.hours.Wednesday !== "null" ? `${data.hours.Wednesday}<br>` : ``}
+            ${data.hours.Thursday !== "null" ? `${data.hours.Thursday}<br>` : ``}
+            ${data.hours.Friday !== "null" ? `${data.hours.Friday}<br>` : ``}
+            ${data.hours.Saturday !== "null" ? `${data.hours.Saturday}<br>` : ``}
+            ${data.hours.Sunday !== "null" ? `${data.hours.Sunday}<br>` : ``}
+
+            <h5><u>PHOTOS TO USE:</u></h5>
+            ${photoLinks}
+            ${!data.logo ? `` : `${logoLinks}<br>`}
+            ${!data.bbb ? `` : `${bbbLinks}<br>`}
+            ${!data.awards ? `` : `${awardOrLogoLinks}<br>`}
             <h5><u>SERVICES:</u></h5>
             ${data.services.split(",").join("<br>")}
             <h5><u>You Can Trust Us to Do the Job for You:</u></h5>
@@ -188,7 +371,8 @@ function generateHTML(data) {
     // #endregion 
 }
 
-`            `
+
+
 function convertToDocx(htmlFilePath, docxFilePath) {
     // #region Convert output.html to a docx
     return new Promise((resolve, reject) => {
@@ -204,7 +388,7 @@ function convertToDocx(htmlFilePath, docxFilePath) {
     // #endregion
 }
 
-async function sendEmail(attachmentPath, company) {
+async function sendEmail(attachmentPath, company, type) {
     // #region Send output.docx in email
 
     let transporter = nodemailer.createTransport({
@@ -221,7 +405,7 @@ async function sendEmail(attachmentPath, company) {
     let mailOptions = {
         from: process.env.user,
         to: process.env.recipient,
-        subject: `New HVAC Survey Submitted for ${company}`,
+        subject: `New ${type} Survey Submitted for ${company}`,
         text: 'Please see the attached document.',
         attachments: [{ path: attachmentPath }]
     };
