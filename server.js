a9914f4e42e02210aaa10f5c96d57dc5fba5395b
spenc3004 <knei294391@alvernia.edu>
2025-04-17 12:03:38 -0400

    HVAC BBB logo map fix.

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.use(express.json())
 const OUTPUT_DIR = path.join(__dirname, 'output');
 if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR);
 
+const bbbImageMap = {
+    "Horizontal black and white": "https://www.themailshark.com/prepress/surveysparrow/hs_bbb/Horizontal%20black%20and%20white.ai",
+    "Vertical black and white": "https://www.themailshark.com/prepress/surveysparrow/hs_bbb/Vertical%20black%20and%20white.ai",
+    "Horizontal blue and white": "https://www.themailshark.com/prepress/surveysparrow/hs_bbb/Horizontal%20blue%20and%20white.ai",
+    "Vertical blue and white with blue A": "https://www.themailshark.com/prepress/surveysparrow/hs_bbb/Vertical%20blue%20and%20white%20with%20blue%20A.ai.ai",
+    "Vertical blue and white with red A": "https://www.themailshark.com/prepress/surveysparrow/hs_bbb/Vertical%20blue%20and%20white%20with%20red%20A.ai",
+    "Vertical blue and white with no A": "https://www.themailshark.com/prepress/surveysparrow/hs_bbb/Vertical%20blue%20and%20white%20no%20A.ai"
+}
+
 app.get("/", (req, res) => {
     res.send('Hello from server.js')
 })
@@ -35,33 +44,6 @@ app.post("/hvac", async (req, res) => {
 
         fs.unlinkSync(htmlFilePath);
         fs.unlinkSync(docxFilePath);
-
-        res.status(200).send('File processed and email sent.');
-    }
-
-    catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-    // #endregion
-});
-
-app.post("/auto", async (req, res) => {
-    // #region Receive JSON from Survey Sparrow
-    try {
-        const data = req.body
-        const company = data.companyName
-        const type = "Auto"
-        const htmlContent = generateAutoHTML(data)
-        const htmlFilePath = path.join(OUTPUT_DIR, 'Auto_Brief.html');
-        const docxFilePath = path.join(OUTPUT_DIR, 'Auto_Brief.docx');
-
-        fs.writeFileSync(htmlFilePath, htmlContent);
-        await convertToDocx(htmlFilePath, docxFilePath);
-        await sendEmail(docxFilePath, company, type);
-
-        // fs.unlinkSync(htmlFilePath);
-        //fs.unlinkSync(docxFilePath);
 
         res.status(200).send('File processed and email sent.');
     }
@@ -151,7 +133,7 @@ function generateHvacHTML(data) {
             <h5><u>LOGOs to Use:</u></h5> 
             ${!data.logo ? `` : `${logoLinks}<br>`}
             ${!data.awards ? `` : `${awardOrLogoLinks}<br>`}
-            ${data.bbb !== "" ? `<p>BBB Logo: ${data.bbb}</p><br>` : ``}
+            ${bbbImageMap[data.bbb] ? `<a href="${bbbImageMap[data.bbb]}" target="_blank"><u><i>View BBB Logo</i></u></a><br>` : ``}
             ${data.otherAwards !== "null" ? `<h6>Other Awards, Affiliations, or Organizations:</h6>  
             ${data.otherAwards.split(",").join("<br>")}` : ``}
             <h5><u>OTHER NOTES:</u></h5> 
@@ -211,6 +193,33 @@ function generateHvacHTML(data) {
     // #endregion 
 }
 
+app.post("/auto", async (req, res) => {
+    // #region Receive JSON from Survey Sparrow
+    try {
+        const data = req.body
+        const company = data.companyName
+        const type = "Auto"
+        const htmlContent = generateAutoHTML(data)
+        const htmlFilePath = path.join(OUTPUT_DIR, 'Auto_Brief.html');
+        const docxFilePath = path.join(OUTPUT_DIR, 'Auto_Brief.docx');
+
+        fs.writeFileSync(htmlFilePath, htmlContent);
+        await convertToDocx(htmlFilePath, docxFilePath);
+        await sendEmail(docxFilePath, company, type);
+
+        // fs.unlinkSync(htmlFilePath);
+        //fs.unlinkSync(docxFilePath);
+
+        res.status(200).send('File processed and email sent.');
+    }
+
+    catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+    // #endregion
+});
+
 function generateAutoHTML(data) {
     // #region insert data dynamically into html
     const photoLinks = data.photos
@@ -226,11 +235,6 @@ function generateAutoHTML(data) {
     const awardOrLogoLinks = data.awards
         .split(",")
         .map(photo => `<a href="${photo}" target="_blank"><u><i>View Logo</i></u></a>`)
-        .join("<br>");
-    // dynamically create "a" tags for bbb logo photos
-    const bbbLinks = data.bbb
-        .split(",")
-        .map(photo => `<a href="${photo}" target="_blank"><u><i>View BBB Logo</i></u></a>`)
         .join("<br>");
 
     const phone = `${data.companyPhone.slice(3, 6)}-${data.companyPhone.slice(7, 10)}-${data.companyPhone.slice(11)}`
@@ -282,14 +286,9 @@ function generateAutoHTML(data) {
             ${data.hours.Friday !== "null" ? `${data.hours.Friday}<br>` : ``}
             ${data.hours.Saturday !== "null" ? `${data.hours.Saturday}<br>` : ``}
             ${data.hours.Sunday !== "null" ? `${data.hours.Sunday}<br>` : ``}
-
             <h5><u>PHOTOS TO USE:</u></h5>
+            ${data.vehicles}<br>
             ${photoLinks}
-            ${!data.logo ? `` : `${logoLinks}<br>`}
-            ${!data.bbb ? `` : `${bbbLinks}<br>`}
-            ${!data.awards ? `` : `${awardOrLogoLinks}<br>`}
-            <h5><u>SERVICES:</u></h5>
-            ${data.services.split(",").join("<br>")}
             <h5><u>You Can Trust Us to Do the Job for You:</u></h5>
             ${data.aseTechnicians}<br>
             ${data.warranties}<br>
@@ -297,72 +296,75 @@ function generateAutoHTML(data) {
             ${data.financing}<br>
             ${data.amenities}<br>
             ${data.sameDayService}<br>
-            ${data.approveFirst}
+            ${data.approveFirst === "true' ? `<p>All Repairs Approved by the Customer Prior to Work Being Done</p>` : ``}"}
             <h5><u>TAGLINES:</u></h5>
             ${data.tagline1 !== "null" ? `${data.tagline1}<br>` : ``}
             ${data.taglines !== "null" ? `${data.taglines.split(",").join("<br>")}` : ``}
-            <h5><u>RATINGS:</u></h5>
+            <h5><u>RATINGS & REVIEWS:</u></h5>
             ${data.stars.google !== "null" ? `Google: ${data.stars.google}<br>` : ``}
             ${data.stars.other1 !== "null" ? `${data.stars.other1}<br>` : ``}
             ${data.stars.other2 !== "null" ? `${data.stars.other2}<br>` : ``}
-            ${data.stars.other3 !== "null" ? `${data.stars.other3}<br>` : ``}
-            ${data.stars.other4 !== "null" ? `${data.stars.other4}` : ``}
-            <h5><u>LOGOs to Use:</u></h5> 
-            ${!data.logo ? `` : `${logoLinks}<br>`}
-            ${!data.bbb ? `` : `${bbbLinks}<br>`}
+            ${data.stars.other3 !== "null" ? `${data.stars.other3}` : ``}
+            <h5><u>SHOP LOGO:</u></h5> 
+            ${!data.logo ? `` : `${logoLinks}`}
+            <h5><u>AWARDS LOGOS:</u></h5>
             ${!data.awards ? `` : `${awardOrLogoLinks}<br>`}
+            ${data.bbb !== "" ? `<p>BBB Logo: ${data.bbb}</p><br>` : ``}
+            ${data.aaa !== "" ? `<p>AAA Logo: ${data.aaa}</p><br>` : ``}
+            ${data.napa !== "" ? `<p>NAPA Logo: ${data.napa}</p><br>` : ``}
+            ${data.caOrTx !== "" ? `<p>CA or TX Logo: ${data.caOrTx}</p><br>` : ``}
             ${data.otherAwards !== "null" ? `<h6>Other Awards, Affiliations, or Organizations:</h6>  
             ${data.otherAwards.split(",").join("<br>")}` : ``}
             <h5><u>OTHER NOTES:</u></h5> 
-            ${data.applicables}
+            
            
-            ${JSON.parse(data.homeOwner) ? `
-                <h6>New Home Owner mailings?</h6> Yes
-                <h6>Radius Offers: </h6>
-                <table>
-                    <tr>
-                        <th>Coupon</th>
-                        <th>Disclaimer</th>
-                    </tr>
-                    ${data.radiusOffers.coupon1 !== "null" ? `
-                    <tr>
-                        <td>${data.radiusOffers.coupon1}</td>
-                        <td>${data.radiusOffers.disclaimer1}</td>
-                    </tr>`: ``}
-                    ${data.radiusOffers.coupon2 !== "null" ? `
-                    <tr>
-                        <td>${data.radiusOffers.coupon2}</td>
-                        <td>${data.radiusOffers.disclaimer2}</td>
-                    </tr>`: ``}
-                    ${data.radiusOffers.coupon3 !== "null" ? `
-                        <tr>
-                            <td>${data.radiusOffers.coupon3}</td>
-                            <td>${data.radiusOffers.disclaimer3}</td>
-                        </tr>`: ``}
-                </table></p> 
-              
-            ` : ``}
+
             <h5><u>COUPONS(There are ${numCoupons()} coupons):</u></h5>        
              <table>
                     <tr>
                         <th>Coupon</th>
                         <th>Disclaimer</th>
                     </tr>
-                    ${data.radiusOffers.coupon1 !== "null" ? `
+                    ${data.coupons.coupon1 !== "null" ? `
                     <tr>
-                        <td>${data.radiusOffers.coupon1}</td>
-                        <td>${data.radiusOffers.disclaimer1}</td>
+                        <td>${data.coupons.coupon1}</td>
+                        <td>${data.coupons.disclaimer1}</td>
                     </tr>`: ``}
-                    ${data.radiusOffers.coupon2 !== "null" ? `
+                    ${data.coupons.coupon2 !== "null" ? `
                     <tr>
-                        <td>${data.radiusOffers.coupon2}</td>
-                        <td>${data.radiusOffers.disclaimer2}</td>
+                        <td>${data.coupons.coupon2}</td>
+                        <td>${data.coupons.disclaimer2}</td>
                     </tr>`: ``}
-                    ${data.radiusOffers.coupon3 !== "null" ? `
+                    ${data.coupons.coupon3 !== "null" ? `
                     <tr>
-                        <td>${data.radiusOffers.coupon3}</td>
-                        <td>${data.radiusOffers.disclaimer3}</td>
+                        <td>${data.coupons.coupon3}</td>
+                        <td>${data.coupons.disclaimer3}</td>
                     </tr>`: ``}
+                    ${data.coupons.coupon4 !== "null" ? `
+                        <tr>
+                            <td>${data.coupons.coupon4}</td>
+                            <td>${data.coupons.disclaimer4}</td>
+                        </tr>`: ``}
+                    ${data.coupons.coupon5 !== "null" ? `
+                        <tr>
+                            <td>${data.coupons.coupon5}</td>
+                            <td>${data.coupons.disclaimer5}</td>
+                        </tr>`: ``}
+                    ${data.coupons.coupon6 !== "null" ? `
+                        <tr>
+                            <td>${data.coupons.coupon6}</td>
+                            <td>${data.coupons.disclaimer6}</td>
+                        </tr>`: ``}
+                    ${data.coupons.coupon7 !== "null" ? `
+                        <tr>
+                            <td>${data.coupons.coupon7}</td>
+                            <td>${data.coupons.disclaimer7}</td>
+                        </tr>`: ``}
+                    ${data.coupons.coupon8 !== "null" ? `
+                        <tr>
+                            <td>${data.coupons.coupon8}</td>
+                            <td>${data.coupons.disclaimer8}</td>
+                        </tr>`: ``}
                 </table></p> 
         </body>
     </body>
